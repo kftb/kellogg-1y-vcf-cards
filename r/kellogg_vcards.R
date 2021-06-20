0a2c10e8472b8776aaaa9b2d9f8e6069b3b42e4f
Kevin Bay <kevin.bay@outlook.com>
2021-06-20 09:10:37 -0500

    feat: Adjust script to deal w Google Sheets input

--- r/kellogg_vcards.R
+++ r/kellogg_vcards.R
@@ -5,14 +5,16 @@ library(dialr)
 library(xlsx)
 
 # Import data
-netids<- read_csv("input/1y_survey.csv") %>% select('netID', 'perm_email')
+netids<- read_csv("input/1y_survey.csv") %>% select('netID', 'perm_email', 'kellogg_email', 'first_name', 'last_name')
 df <- read_csv("input/1Y Class of 2021 Directory - 1Y Class of 2021 Directory.csv", skip=3, 
                col_types = c('Phone Number' = 'c',
                              'WhatsApp Number' = 'c'))
 names(df) <- c('x1', 'first_name', 'last_name', 'student_jv', 'perm_email', 'fixed_country_code', 'fixed_phone', 'wa_country', 'fixed_wa_phone', 'industry', 'function', 'employer_clean',  'geo_country','geo_city_primary', 'geo_city_secondary' )
 
+df <- df %>% mutate(mergeid = paste0(first_name, last_name))
+netids <- netids %>% mutate(mergeid = paste0(first_name, last_name)) %>% select('netID', 'kellogg_email', 'mergeid')
 # Merge netids with other dataset
-df<- left_join(df, netids, by='perm_email')
+df <- left_join(df, netids, by="mergeid")
 
 # Set option for dialr format
 getOption("dialr.format")
@@ -21,7 +23,8 @@ options(dialr.format = "INTERNATIONAL")
 df_new <- df %>%
   
   # Format email addresses
-  mutate(perm_email_corr = tolower(perm_email)) %>% 
+  mutate(perm_email_corr = tolower(perm_email),
+         netID = tolower(netID)) %>% 
   
   # Reformat numbers
   mutate_at("fixed_phone", ~phone(., fixed_country_code)) %>%
@@ -39,7 +42,7 @@ df_new <- df %>%
                  wa_final = format))
   
 
-write.csv(df_new, "output/1y_dir_output_db.csv")
+write.csv(df_new, "output/1y_dir_output.csv")
 backup_name = format(Sys.time(), 'output/backups/%y%m%d_%H%M%S_1y_dir_output_db.csv')
 write.csv(df_new, backup_name)
 
